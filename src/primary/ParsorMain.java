d21b208690be6e93166cb6edae2b0e7a46679b1f
Tony <AABoyles@gmail.com>
2012-09-30 21:38:59 -0400

    Cut out some Regexs that were specific to Andrew's Application

--- src/primary/ParsorMain.java
+++ src/primary/ParsorMain.java
@@ -1,17 +1,4 @@
 package primary;
-//Parsor - A simple scraper for HTML and text files, which generates a dataset based on the sentences each file contains.
-//by Tony Boyles - AABoyles@gmail.com - http://nortalktoowise.com/
-/*  This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-<http://www.gnu.org/licenses/>.*/
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -21,28 +8,30 @@ import java.io.PrintStream;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class ParsorMain {
+
 	public static void main(String[] args) throws Exception{
 
 		File[] files;
-		String[] meta;
 		StringBuilder text, sentence;
 		String line, title = "";
 		int sentenceNumber=0, numSentences=1, initialLineLength=0;
 		double sensitivity = .3;
 		BufferedReader reader = null;
-		JFileChooser fileopen = new JFileChooser();
 
+		JFileChooser fileopen = new JFileChooser();
 		fileopen.addChoosableFileFilter(new FileNameExtensionFilter("HTML files", "htm", "html"));
 		fileopen.setMultiSelectionEnabled(true);
-
-		JFileChooser filesave = new JFileChooser();
-		filesave.addChoosableFileFilter(new FileNameExtensionFilter("CSV files", "csv"));
 		int ret = fileopen.showDialog(null, "Open file");
 
 		if (ret == JFileChooser.APPROVE_OPTION){
 			JOptionPane.showMessageDialog(null,"All Files Loaded Successfully.");
+
+			JFileChooser filesave = new JFileChooser();
+			filesave.addChoosableFileFilter(new FileNameExtensionFilter("CSV files", "csv"));
 			int ret2 = filesave.showSaveDialog(null);
+
 			if (ret2 == JFileChooser.APPROVE_OPTION){
 				PrintStream p=new PrintStream(new FileOutputStream(filesave.getSelectedFile()));
 				files = fileopen.getSelectedFiles();
@@ -52,7 +41,6 @@ public class ParsorMain {
 					text = new StringBuilder("");
 					sentenceNumber=0;
 					numSentences=1;
-					//meta = files[i].getName().split("_");
 					reader = new BufferedReader(new FileReader(files[i]));
 					line = reader.readLine().trim();
 
@@ -87,25 +75,14 @@ public class ParsorMain {
 
 					if (text.length() > sensitivity * initialLineLength){
 
-						text=new StringBuilder(text.toString()
-								.replaceAll("\\.\\.\\.", "\\. ").replaceAll("\"", "")
-								.replaceAll(",", "").replaceAll("St\\. ", "St ").replaceAll("   ", "\\. ")
-								.replaceAll("U\\.S\\. ", "US ").replaceAll("U\\.N\\. ", "UN ")
-								.replaceAll("N\\.Y\\. ", "NY ").replaceAll("D\\.C\\. ", "DC ")
-								.replaceAll("[A-Z]\\. ", " ").replaceAll("[\r\n\t]", "\\. ")
-								.replaceAll("Rep\\. ", "Rep ").replaceAll("Sen\\. ", "Sen ")
-								.replaceAll("Sens\\. ", "Senators ").replaceAll("Reps. ", "Representatives ")
-								.replaceAll("Wash\\. ", "Wash ").replaceAll("Mass\\. ", "Mass ")
-								.replaceAll("[“”]", "''"));
-
+						text=new StringBuilder(text.toString());
+						
 						for(int j = 0; j < text.toString().split("[.!?][ \n\r]").length; j++){
 							sentence = new StringBuilder(text.toString().split("[.!?][ \n\r]")[j]);
 							if(sentence.toString().split("\\s").length>3){numSentences++;}}
 
 						if(!title.isEmpty()){
 							p.println(
-									//meta[0] + "," + meta[3] + "," + meta[1] + "," 
-									//+ meta[2] + "," + meta[4].charAt(0) + ","
 									numSentences + ",1," + ++sentenceNumber + "," 
 									+ title.trim().split(" ").length + "," 
 									+ title.trim().length() + "," 
@@ -116,8 +93,6 @@ public class ParsorMain {
 							sentence = new StringBuilder(text.toString().split("[.!?][ \n\r]")[j]);
 							if(sentence.toString().split("\\s").length>3){
 								p.println(
-										//meta[0] + "," + meta[3] + "," + meta[1] + "," 
-										//+ meta[2] + "," + meta[4].charAt(0) + ","
 										numSentences + ",0," + ++sentenceNumber + "," 
 										+ sentence.toString().trim().split("\\s").length + "," 
 										+ sentence.toString().trim().length() + "," 
@@ -132,3 +107,4 @@ public class ParsorMain {
 			}
 		}
 	}
+}
